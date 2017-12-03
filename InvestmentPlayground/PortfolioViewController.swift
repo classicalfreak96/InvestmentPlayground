7c2dc039002a91a423a8134cd851aa7e90647ae8
Michelle Xu <michelle.xu@wustl.edu>
2017-12-02 23:59:27 -0600

    Popup for selling

--- InvestmentPlayground/PortfolioViewController.swift
+++ InvestmentPlayground/PortfolioViewController.swift
@@ -43,16 +43,54 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
         return 1 //one to say change
     }
     
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?{
+        let sell = UITableViewRowAction(style: .normal, title: "Sell") {(action, indexpath) in
+            print("CAN SELL")
+            let alert = UIAlertController(title: "Sell " + self.stocks[indexPath.row].ticker + " stocks", message: "Enter number of shares: ", preferredStyle: .alert)
+            alert.addTextField { (textField) in
+                textField.text = " "
+            }
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+                let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+                if let text: String = textField?.text {
+                    let trimmedString = Int(text.trimmingCharacters(in: .whitespaces))
+                    self.stocks[indexPath.row].numShares = self.stocks[indexPath.row].numShares - trimmedString!
+                    //NEEDS TO UPDATE STOCK IN DATABASE WITH NEW NUMBER OF STOCKS
+                    //print(self.stockHold[0].numShares)
+                    let username = UserDefaults.standard.string(forKey: "username")
+                    if let user = username {
+                        print("inside user = username")
+                        //self.addStock(username: user, ticker: self.tickerName, numShares: self.stockHold[0].numShares)
+                    }
+                }
+                //self.performSegue(withIdentifier: "toPortfolioView", sender: self)
+                self.portfolioTable.reloadData()
+            }))
+
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        sell.backgroundColor = UIColor(red: 0.298, green: 0.851, blue: 0.3922, alpha: 1.0);
+        return [sell]
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { //help from https://www.ralfebert.de/tutorials/ios-swift-uitableviewcontroller/#data_swift_arrays
         let cell1 = tableView.dequeueReusableCell(withIdentifier: "portfolioCell", for: indexPath) as UITableViewCell
-        cell1.textLabel?.text = stocks[indexPath.row].ticker + "(" + String(stocks[indexPath.row].numShares) + ")"
+        cell1.textLabel?.text = self.stocks[indexPath.row].ticker + "(" + String(self.stocks[indexPath.row].numShares) + ")"
         return cell1
     }
     
     // This function pulls all of the stocks for a given user and reloads
     // the table with the tickers of the stocks
     func getStocksForUser(username: String) {
-        db.collection("stocks").whereField("username", isEqualTo: username)
+        self.db.collection("stocks").whereField("username", isEqualTo: username)
             .getDocuments() { [unowned self] (querySnapshot, err) in
                 if let err = err {
                     print("Error getting documents: \(err)")
@@ -71,3 +109,4 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
 }
+
